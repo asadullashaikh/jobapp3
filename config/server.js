61d477366e0401b5481b55f3a53c933c6c6a0fc7
Ubuntu <ubuntu@ip-172-31-13-114.ap-south-1.compute.internal>
2022-04-27 06:20:34 +0000

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54fb5ba6405c7b044be3c19ae4995153'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '027e8722-09e7-43b7-99a3-a9bca7cf81e0'
+};
